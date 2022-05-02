1683a4903c29aff6fb940c41725dd144efc6ec59
AlainBernier <95882671+AlainBernier@users.noreply.github.com>
2022-05-02 17:00:26 -0400

    Creation des fichiers tests

--- Labo 1Tests/GameStatsTestCase.swift
+++ Labo 1Tests/GameStatsTestCase.swift
@@ -1,15 +1,14 @@
 //
-//  Labo_1Tests.swift
+//  GameStatsTestCase.swift
 //  Labo 1Tests
 //
-//  Created by Vincent Leduc on 2021-06-30.
-//  Copyright © 2021 Ahuntsic. All rights reserved.
+//  Created by Alain Bernier on 2022-05-02.
+//  Copyright © 2022 Ahuntsic. All rights reserved.
 //
 
 import XCTest
-@testable import Lab // MARK: - FIXME
 
-class Labo_1Tests: XCTestCase {
+class GameStatsTestCase: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,11 +21,14 @@ class Labo_1Tests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
---
+++ Labo 1Tests/GameTestCase.swift
@@ -0,0 +1,16 @@
+//
+//  GameTestCase.swift
+//  Labo 1Tests
+//
+//  Created by Alain Bernier on 2022-05-02.
+//  Copyright © 2022 Ahuntsic. All rights reserved.
+//
+
+import XCTest
+@testable import Labo_1
+
+class GameTestCase: XCTestCase {
+    func testGivenTwoTrueQuestions_WhenAnsweringTrue_ThenScoreIsOneAndGameOnGoing() {
+        
+    }
+}
